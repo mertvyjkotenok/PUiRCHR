28bf2899605065c781ea3b4320e317f3bea899f8
mertvyjkotenok <sm4.dsha@gmail.com>
2025-09-09 11:15:14 +0300

    first

---
+++ Deligirovanie/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Deligirovanie
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Cooper Black", 25.8000011F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(77, 40);
+            label1.Name = "label1";
+            label1.Size = new Size(655, 50);
+            label1.TabIndex = 0;
+            label1.Text = "КРУТОЙ ТЕСТ ДЕЛЕГИРОВАНИЕ";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(77, 156);
+            button1.Name = "button1";
+            button1.Size = new Size(122, 50);
+            button1.TabIndex = 1;
+            button1.Text = "ТЕСТ 1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(342, 156);
+            button2.Name = "button2";
+            button2.Size = new Size(122, 50);
+            button2.TabIndex = 2;
+            button2.Text = "ТЕСТ 2";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(610, 156);
+            button3.Name = "button3";
+            button3.Size = new Size(122, 50);
+            button3.TabIndex = 3;
+            button3.Text = "ТЕСТ 3";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(255, 128, 255);
+            ClientSize = new Size(800, 450);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            Name = "Form1";
+            Text = "ООО \"Пущенцы\"";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+    }
+}
---
+++ Deligirovanie/Form2.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Deligirovanie
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            popa4 = new CheckedListBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Courier New", 25.8000011F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label1.Location = new Point(307, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(178, 50);
+            label1.TabIndex = 0;
+            label1.Text = "ТЕСТ 1";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Courier New", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label2.Location = new Point(321, 59);
+            label2.Name = "label2";
+            label2.Size = new Size(150, 31);
+            label2.TabIndex = 1;
+            label2.Text = "ВОПРОС 1";
+            // 
+            // popa4
+            // 
+            popa4.FormattingEnabled = true;
+            popa4.Location = new Point(272, 167);
+            popa4.Name = "popa4";
+            popa4.Size = new Size(267, 158);
+            popa4.TabIndex = 2;
+            popa4.SelectedIndexChanged += checkedListBox2_SelectedIndexChanged;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(255, 128, 255);
+            ClientSize = new Size(800, 450);
+            Controls.Add(popa4);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form2";
+            Text = "ТЕСТ 1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private CheckedListBox popa4;
+    }
+}
